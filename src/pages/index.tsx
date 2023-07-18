4cf816ed22168d9d78434216d45ffff5f0ff976d
Alvin <alvinmartindwrk@gmail.com>
2023-07-18 11:41:44 +0700

    add link to github source code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import Layout from '@/components/layout';
+import { buttonVariants } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function HomePage() {
   return (
     <Layout>
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-4">
         Hi, admin.
       </h1>
+      <Link
+        href="https://github.com/alvinmdj/dealls-product-dashboard"
+        target="_blank"
+        className={buttonVariants({ variant: 'default' })}
+      >
+        <Image
+          src="/github-mark-white.svg"
+          alt=""
+          width={16}
+          height={16}
+          className="mr-2"
+        />
+        Source code
+      </Link>
     </Layout>
   );
 }
